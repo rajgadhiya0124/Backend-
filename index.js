89ee747cb434a96e39cb685e08f4931b64bb8960
Raj Gadhiya <rajgadhiya0124@gmail.com>
2025-04-10 11:59:33 +0530

    commit

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
-app.get("/login",(req,res)=>{
-    res.send("you are loggined")
-})
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);    
