18abb8e9b0be71623b76f7551d7df63d31b39080
Raj Gadhiya <rajgadhiya0124@gmail.com>
2025-04-12 15:15:41 +0530

    database setup

---
+++ Project/src/index.js
@@ -0,0 +1,29 @@
+import connectDB from "./db/DBconnection.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"/.env"
+})
+connectDB()
+
+
+
+
+// import express from "express"
+// const app = express()
+
+// (async()=>{
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URL}/${DB_Name}`)
+//     app.on("error",(error)=>{
+//         console.log("ERROR",error)
+//         throw error
+//     })
+
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`App is listining on port ${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.log("Error",error);
+//   }
+// })()
